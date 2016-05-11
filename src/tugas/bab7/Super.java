e66637a5c46e688dee820f5b30e9112787e5847d
FarisGenis <faaris513@gmail.com>
2016-05-12 02:57:32 +0700

    10

--- src/tugas/bab7/Super.java
+++ src/tugas/bab7/Super.java
@@ -9,10 +9,11 @@ public abstract class Super {
             Pomade = 200000,
             Comb = 45000;
 
-     public void getBill(String SG) {
+    public void getBill(String SG) {
         System.out.println("Service/Goods     : " + SG);
         System.out.println("Price             : " + "Rp " + Price);
         System.out.println("Discount          : " + "Rp " + (Price - Pay));
+        System.out.println("___________________________________________");
         System.out.println("Pay               : " + "Rp " + Pay);
     }
 
